857f96cae7af3754a6274bb2834956253ffcf919
susana.bustosmartinez <susana.bustos88@gmail.com>
2017-11-16 19:36:29 +0100

    Version inicial del proyecto. Se ha modificado las clases de datos moviendolas a una subcarpeta dto y renombrandolos con el sufijo DTO

---
+++ src/main/java/com/mycorp/support/PolizaBasicoFromPolizaBuilder.java
@@ -0,0 +1,44 @@
+package com.mycorp.support;
+
+import com.mycorp.dto.PolizaDTO;
+
+import util.datos.PolizaBasico;
+
+public class PolizaBasicoFromPolizaBuilder {
+    /** The poliza. */
+    private PolizaDTO poliza;
+
+    /**
+     * With poliza.
+     *
+     * @param poliza
+     *            the poliza
+     * @return the poliza basico from poliza builder
+     */
+    public PolizaBasicoFromPolizaBuilder withPoliza( final PolizaDTO poliza ) {
+        this.poliza = poliza;
+        return this;
+    }
+
+    /**
+     * Builds the.
+     *
+     * @return the poliza basico
+     */
+    public PolizaBasico build() {
+
+        PolizaBasico polizaBasico = null;
+
+        if( null != poliza ) {
+            polizaBasico = new PolizaBasico();
+            polizaBasico.setCompania( poliza.getCompania() );
+            polizaBasico.setNumPoliza( poliza.getNumPoliza() );
+            polizaBasico.setNumColectivo( poliza.getNumColectivo() );
+            if (poliza.getPlanInfo()!= null && poliza.getPlanInfo().getCode()!= null) {
+                polizaBasico.setPlan(Integer.valueOf(poliza.getPlanInfo().getCode()));
+            }
+        }
+
+        return polizaBasico;
+    }
+}
